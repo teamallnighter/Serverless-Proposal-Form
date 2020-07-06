96a945ba9ae44e924dc7610508a87a7f337518a9
Chris Connelly <connellychris@me.com>
2020-07-06 02:19:32 -0600

    RE Do

--- script.js
+++ script.js
@@ -1,98 +1,23 @@
-var position = 1;     // Defines the index position.
-var name = "";        // Will hold the name once filled in.
-var email = "";       // Will hold the email once filled in.
-var companyName = ""; // Will hold the company name once filled in.
-var website = "";     // Will hold the website once filled in.
-var projectType = ""; // Will hold the project type once filled in.
-var budget = "";      // Will hold the budget once filled in.
-var description = ""; // Will hold the description once filled in.
-
-// When clicking the next button...
-$("#next-button").click(function() {
-  // Log the current position
-  console.log("Current position is: " + position);
-  // Increment the position
-  position++;
-  // Log the new position
-  console.log("Incremented to: " + position);
-  // Animate scrolling to the slide with the index of position
+$("#begin").click(function() {
   $('html, body').animate({
-    scrollTop: $(".slide:nth-child("+position+")").offset().top
+    scrollTop: $("#you").offset().top
   }, 1000);
-  // ZOMG!!1!! it worked.
-  console.log("Scroll function complete.");
-  // If the position is 4
-  if (position === 4) {
-    // Disable the next button
-    $("#next-button").prop("disabled",true);
-    $("#next-button").addClass("disabled");
-    $("#back-button").prop("disabled",true);
-    $("#back-button").addClass("disabled");
-  }
-  // If the position is 2
-  if (position === 2) {
-    // Enable the back button
-    $("#back-button").prop("disabled",false);
-    $("#back-button").removeClass("disabled");
-  }
 });
 
-// Same as above but backwards.
-$("#back-button").click(function() {
-  position--;
+$("#to-company").click(function() {
   $('html, body').animate({
-    scrollTop: $(".slide:nth-child("+position+")").offset().top
+    scrollTop: $("#company").offset().top
   }, 1000);
-  if (position === 4) {
-    $("#back-button").prop("disabled",true);
-    $("#back-button").addClass("disabled");
-    $("#next-button").prop("disabled",true);
-    $("#next-button").addClass("disabled");
-  }
-  if (position === 1) {
-    $("#back-button").prop("disabled",true);
-    $("#back-button").removeClass("disabled");
-  }
 });
 
-// Submit button functionality -- There is NO validation attached to this right now.
-$("#submit-button").click(function() {
-
-  if (!$("form#project-proposal").checkValidity) {
-    position = 2;
-    $('html, body').animate({
-      scrollTop: $(".slide:nth-child("+position+")").offset().top
-    }, 1000);
-    $("#next-button").prop("disabled",false);
-    $("#next-button").removeClass("disabled");
-  } else {
-    // Grab variables from form
-    name = $("#name").val();
-    email = $("#email").val();
-    companyName = $("#companName").val();
-    website = $("#website").val();
-    projectType = $("#projectType").val();
-    budget = $("#budget").val();
-    description = $("#description").val();
-
-    // Log the variables that were grabbed
-    console.log("NAME: " + name);
-    console.log("EMAIL: " + email);
-    console.log("COMPANY NAME: " + companyName);
-    console.log("WEBSITE: " + website);
-    console.log("PROJECT TYPE: " + projectType);
-    console.log("BUDGET: " + budget);
-    console.log("DESCRIPTION: " + description);
-
-    // Hide the slides and controls, bring in the Thank you message
-    $(".slide").fadeOut(1000);
-    $("#controls").fadeOut(1000);
-    $(".thank-you").fadeIn(2000);
-  }
-
+$("#to-project").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#project").offset().top
+  }, 1000);
 });
 
-// Hide the thank you message and disable the back button from the start
-$(".thank-you").hide();
-$("#back-button").prop("disabled",false);
-$("#back-button").addClass("disabled");
+$("#my-form").submit(function(e) {
+  e.preventDefault();
+
+
+
---
+++ submit.js
@@ -0,0 +1,8 @@
+$("#my-form").submit(function(e) {
+    e.preventDefault();
+
+var $form = $(this);
+$.post($form.attr("action"), $form.serialize()).then(function() {
+  alert("Thank you!");
+});
+});
